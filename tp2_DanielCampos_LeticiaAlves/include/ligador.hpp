068ae60fd103dcfa740a8a40966dcc0b79818886
Pendulun <danielcampos14@hotmail.com.br>
2021-08-08 19:55:34 -0300

    Movendo métodos do Assembler para o Linker

--- tp2_DanielCampos_LeticiaAlves/include/ligador.hpp
+++ tp2_DanielCampos_LeticiaAlves/include/ligador.hpp
@@ -1,6 +1,40 @@
 #ifndef LIGADOR_H
 #define LIGADOR_H
 
-// TODO: structs, classes, e outras definições.
+#include <fstream>
+#include <string>
+#include <list>
+#include <unordered_map>
+
+class Ligador{
+    private:
+        std::ofstream* saida;
+
+        const unsigned int TAMANHOPILHA = 1000;
+        unsigned int tamanhoPrograma;
+        unsigned int endCarregamento;
+        unsigned int posAP;
+        unsigned int entryPoint;
+        bool definiuEntryPoint;
+
+        
+        void escreveCabecalhoArquivoSaida();
+        void escreveInformacoesArquivoSaida();
+        void escreveInstrucaoNoArquivoSaida(const std::string, const  std::list<std::string>);
+        void verificaEDefineEntryPoint();
+        void defineInformacoesArquivoSaida();
+
+        int calculaEnderecoRelativo(unsigned int pc, unsigned int endereco);
+
+    public:
+        Ligador(std::ofstream& saida);
+        ~Ligador();
+
+        void lerTamanhoETabelaDoArquivo(std::ifstream& arquivoEntrada, std::string nomeArquivo);
+        void completarInstrucoesDoArquivo(std::ifstream& arquivoEntrada, std::string nomeArquivo);
+        void passo1();
+        void passo2();
+
+};
 
 #endif
--- tp2_DanielCampos_LeticiaAlves/src/ligador/ligador.cpp
+++ tp2_DanielCampos_LeticiaAlves/src/ligador/ligador.cpp
@@ -1,3 +1,60 @@
 #include "ligador.hpp"
 
-// TODO: implementação do editor de ligação
+Ligador::Ligador(std::ofstream& saida){
+    this->saida = &saida;
+    this->posAP=0;
+    this->tamanhoPrograma=0;
+    this->entryPoint=0;
+    this->definiuEntryPoint = false;
+    this->endCarregamento=0;
+}
+
+void Ligador::escreveCabecalhoArquivoSaida(){
+    (*this->saida)<<"MV-EXE";
+    (*this->saida)<<std::endl;
+    (*this->saida)<<std::endl;
+}
+
+void Ligador::verificaEDefineEntryPoint(){
+    if(!this->definiuEntryPoint){
+        //this->entryPoint = this->LC;
+        this->definiuEntryPoint=true;
+    }
+}
+
+void Ligador::escreveInformacoesArquivoSaida(){
+    *this->saida<<std::to_string(this->tamanhoPrograma).append(" ");
+    *this->saida<<std::to_string(this->endCarregamento).append(" ");
+    *this->saida<<std::to_string(this->posAP).append(" ");
+    *this->saida<<std::to_string(this->entryPoint);
+    *this->saida<<std::endl;
+    *this->saida<<std::endl;
+}
+
+void Ligador::escreveInstrucaoNoArquivoSaida(const std::string codigoOperacao, const std::list<std::string> operandos){
+    *this->saida<<codigoOperacao<<" ";
+    for(std::string operando : operandos){
+        *this->saida<<operando.append(" ");
+    }
+}
+
+void Ligador::defineInformacoesArquivoSaida(){
+    //this->tamanhoPrograma = this->LC;
+    this->endCarregamento=0;
+    this->posAP = this->endCarregamento + this->tamanhoPrograma + this->TAMANHOPILHA;
+
+}
+
+void Ligador::passo1(){
+
+}
+
+void Ligador::passo2(){
+
+}
+
+
+int Ligador::calculaEnderecoRelativo(unsigned int LC, unsigned int endereco){
+    int enderecoRelativo = endereco - (LC);
+    return enderecoRelativo;
+}
